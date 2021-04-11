ca254fd42327c81bea97dff03d67628cc639fcf4
Artem Poponin <artempoponin1@gmail.com>
2021-04-11 23:01:55 +0300

    Inintial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        int current_balance = 100;
+        long bonus;
+        long balance_replenishment = 1190;
+        if (balance_replenishment >= 1000){
+            bonus = balance_replenishment/100;
+        } else {
+            bonus = 0;
+        }
+        long new_balance = current_balance + balance_replenishment + bonus;
+        System.out.println(new_balance);
+
+    }
+}
