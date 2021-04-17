77e6149885cbe16db6dba86faef765ca00de14e6
ArtemPoponin <81485611+ArtemPoponin@users.noreply.github.com>
2021-04-17 18:55:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int currentBalance = 100;
+        int balance = 100;
         long bonus;
         long balanceReplenishment = 1190;
-        if (balanceReplenishment >= 1000){
+        if (balanceReplenishment > 1000){
             bonus = balanceReplenishment/100;
         } else {
             bonus = 0;
         }
-        long balance = currentBalance + balanceReplenishment + bonus;
+        long balance = balance + balanceReplenishment + bonus;
         System.out.println(new_balance);
 
     }
